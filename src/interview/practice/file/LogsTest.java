86b8bb4cd885c7750d19f7f31e3dfb4bc43b8df4
damonjt <dolphin422@163.com>
2017-03-09 23:49:40 +0800

    初次导入

---
+++ src/interview/practice/file/LogsTest.java
@@ -0,0 +1,34 @@
+package interview.practice.file;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogsTest {
+
+    // 首先获得日志记录这个对象
+    static private Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
+
+    @Test
+    public void slf4jTest() {
+            //记录error信息
+    
+            logger.error("[error message]");
+    
+            // 记录info，还可以传入参数
+    
+            logger.info("[info message],{},{}", false,new Slf4jTest());
+    
+            // 记录deubg信息
+    
+            logger.debug("[debug message]");
+    
+            // 记录trace信息
+    
+            logger.trace("[trace message]");
+    
+            System.out.println("hello world");
+    
+        }
+    
+}
